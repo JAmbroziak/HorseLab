9b09581336b7f70c6f1ca5bd16f40e3a4311f9d3
BT_1N3_17@BKTECH.LOCAL <BT_1N3_17@BKTECH.LOCAL>
2018-11-15 14:49:15 -0500

    Initial commit

---
+++ src/Horse.java
@@ -0,0 +1,6 @@
+public interface Horse {
+    /** @returns the horse's name*/
+    String getName();
+    /** @returns the horse's weight*/
+    double getWeight();
+}
---
+++ src/Mustang.java
@@ -0,0 +1,17 @@
+public class Mustang implements Horse {
+    private String Name;
+    private double Weight;
+
+    public Mustang(String Name, double Weight){
+        this.Name = Name;
+        this.Weight = Weight;
+    }
+
+    public String getName(){
+        return this.Name;
+    }
+
+    public double getWeight(){
+        return this.Weight;
+    }
+}
